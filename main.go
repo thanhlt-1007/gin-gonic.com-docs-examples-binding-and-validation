b1fc5106fef5cc8de08b295d2310c3b0ef3a8a96
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-01-24 19:22:57 +0700

    Add POST /login

--- main.go
+++ main.go
@@ -5,17 +5,44 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func getPingHandler(context *gin.Context) {
+type Login struct {
+    User string `json:"user" binding:"required"`
+    Password string `json:"password" binding:"required"`
+}
+
+func postLoginHandler(context *gin.Context) {
+    var login Login
+    err := context.ShouldBind(&login)
+    if err != nil {
+        context.JSON(
+            http.StatusBadRequest,
+            gin.H {
+                "error": err.Error(),
+            },
+        )
+        return
+    }
+
+    if login.User != "admin" || login.Password != "Aa@123456" {
+        context.JSON(
+            http.StatusUnauthorized,
+            gin.H {
+                "error": "Unauthorized",
+            },
+        )
+        return
+    }
+
     context.JSON(
         http.StatusOK,
         gin.H {
-            "message": "pong",
+            "message": "You are logged in",
         },
     )
 }
 
 func main() {
     engine := gin.Default()
-    engine.GET("/ping", getPingHandler)
+    engine.POST("/login", postLoginHandler)
     engine.Run()
 }
